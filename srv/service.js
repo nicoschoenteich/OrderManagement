870c2b95f8664c629083ca764fcf780d6d0c9e7a
Shrinivasan Neelamegam <shrinivasan.neelamegam@sap.com>
2024-01-09 13:24:10 +0530

    CRUD operations on Orders and Items

--- srv/service.js
+++ srv/service.js
@@ -2,8 +2,7 @@
 const cds = require('@sap/cds')
 
 module.exports = function () {
-
-    const { Orders, OrderItems } = cds.entities("OrderManagement")
+    const { Orders } = cds.entities("OrderManagement")
 
     this.on("purchaseOrder", async req => {
         const ID = req.data.order
@@ -11,13 +10,5 @@ module.exports = function () {
         if (!order) return req.error (404,`Order ${ID} doesn't exist`)
         await UPDATE (Orders, ID) .with ({ purchased: true, date: new Date() })
         return await SELECT (Orders, ID)
-    })
-
-    this.on("purchaseOrderItems", async req => {
-        const ID = req.data.order
-        const ItemID = req.data.item
-        // let items = await SELECT (Orders, ID)
-        // if (!items) return req.error (404,`Items ${ID} doesn't exist`)
-        return await SELECT(OrderItems, ID, ItemID)
     })
 }
